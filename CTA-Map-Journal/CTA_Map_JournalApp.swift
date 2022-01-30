8c85d668ebe0bb132896b7adc6cc8bcb2ec27e5e
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-01-30 16:20:31 -0600

    Initial Commit

---
+++ CTA-Map-Journal/CTA_Map_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTA_Map_JournalApp.swift
+//  CTA-Map-Journal
+//
+//  Created by Ivette Fernandez on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CTA_Map_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
