7bfbb609db6f9e136a04674978ef84611c05b8d5
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-01-30 17:07:13 -0600

    connected firestore

--- CTA-Map-Journal/CTA_Map_JournalApp.swift
+++ CTA-Map-Journal/CTA_Map_JournalApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CTA_Map_JournalApp: App {
+    init () {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
