94447b8417f3dd3aa3c3a824bb88209a40a992ea
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2022-01-31 00:23:34 -0600

    edits to HomePageView

--- CTA-Map-Journal/Views/HomePageView.swift
+++ CTA-Map-Journal/Views/HomePageView.swift
@@ -6,10 +6,69 @@
 //
 
 import SwiftUI
+import CoreLocationUI
+import MapKit
 
 struct HomePageView: View {
+    @State var nearestTrainStation = ""
+    var trainLines: [String:String] = [
+        "red": "Red Line",
+        "blue": "Blue Line",
+        "g": "Green Line",
+        "brn": "Brown Line",
+        "p": "Purple Line",
+        "y": "Yellow Line",
+        "pnk": "Pink Line",
+        "o": "Orange Line"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack (spacing: 0) {
+            Text("CTA Map Journal")
+                .font(.title)
+            .fontWeight(.bold)
+            Spacer()
+            ZStack {
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                HStack {
+                    Text("Find Nearest Station: ")
+                        .font(.headline)
+                        .foregroundColor(Color.white)
+                        
+                    LocationButton (.currentLocation) {
+                        // use function from NearestTrainStation
+                    } .foregroundColor(.white)
+                    
+                }
+            }
+            ZStack {
+                Rectangle()
+                    .fill(Color(.systemGray))
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                Text("Placeholder \(nearestTrainStation)")
+            }
+            ZStack (alignment: .leading) {
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(maxWidth: .infinity, maxHeight: 50)
+                Text("Select a Train Line:")
+                    .font(.headline)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.leading)
+            }
+            List {
+                ForEach(trainLines.sorted(by: >), id: \.key) {key, value in
+                        Text(value)
+                        .listRowBackground(Color(.systemGray5))
+                    
+
+                }
+            }
+            
+            Spacer()
+
+        }
     }
 }
 
